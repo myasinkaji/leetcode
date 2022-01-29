76f6d2dede8bfd446e7735fb7d7f77658846a7fa
myasinkaji <myasinkaji@gmail.com>
2022-01-29 17:18:49 +0330

    #weekly-contest-277 2149. Rearrange Array Elements by Sign WIP-2

--- app/src/main/java/ir/home/tutorial/algorithm/leetcode/contest/weeklycontest277/q2/Solution.java
+++ app/src/main/java/ir/home/tutorial/algorithm/leetcode/contest/weeklycontest277/q2/Solution.java
@@ -1,19 +1,59 @@
 package ir.home.tutorial.algorithm.leetcode.contest.weeklycontest277.q2;
 
-import java.util.LinkedList;
 
 public class Solution {
+    private class IntLinkedList {
+        private IntNode head;
+        private IntNode tail;
+
+        private int remove() {
+            int value = head.value;
+            head = head.next;
+            return value;
+        }
+
+        private void add(int value) {
+            if (head == null)
+                head = tail = new IntNode(value, null);
+            else {
+                tail.next = new IntNode(value, null);
+                tail = tail.next;
+            }
+        }
+
+    }
+
+    private static class IntNode {
+        private final int value;
+        private IntNode next;
+
+        public IntNode(int value, IntNode next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public void setNext(IntNode next) {
+            this.next = next;
+        }
+    }
+
+
     public int[] rearrangeArray(int[] nums) {
-        final var positives = new LinkedList<Integer>();
-        final var negatives = new LinkedList<Integer>();
-        for (int num : nums)
+        short len = (short) (nums.length >> 1);
+        final int[] positives = new int[len];
+        final int[] negatives = new int[len];
+        int posIndex = 0, negIndex = 0;
+        for (int i = 0; i <nums.length ; i++) {
+            int num = nums[i];
             if (num < 0)
-                negatives.add(num);
+                negatives[negIndex++] = num;
             else
-                positives.add(num);
-        for (int i = 0; i < (nums.length / 2); i++) {
-            nums[2 * i] = positives.remove();
-            nums[2 * i + 1] = negatives.remove();
+                positives[posIndex++] = num;
+        }
+        posIndex = negIndex = 0;
+        for (int i = 0; i < len; i++) {
+            nums[i << 1] = positives[posIndex++];
+            nums[(i << 1) + 1] = negatives[negIndex++];
         }
         return nums;
     }
