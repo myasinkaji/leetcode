97ce0568b96c6c42534a472274db8f4966a2b97e
myasinkaji <myasinkaji@gmail.com>
2022-02-06 18:00:25 +0330

    #weekly-contest-279 2164. Sort Even and Odd Indices Independently MBR(Must be Refactored)

---
+++ app/src/main/java/ir/home/tutorial/algorithm/leetcode/contest/weeklycontest279/p1/Solution.java
@@ -0,0 +1,36 @@
+package ir.home.tutorial.algorithm.leetcode.contest.weeklycontest279.p1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public int[] sortEvenOdd(int[] nums) {
+        final Integer[] odds = new Integer[(int) Math.floor(nums.length / 2d)];
+        final Integer[] evens = new Integer[(int) Math.ceil(nums.length / 2d)];
+
+        if (nums.length <= 2)
+            return nums;
+
+        int i;
+        for (i = 0; i < nums.length / 2; i++) {
+            evens[i] = nums[2 * i];
+            odds[i] = nums[2 * i + 1];
+        }
+
+        if (nums.length % 2 == 1)
+            evens[i] = nums[nums.length - 1];
+
+        Arrays.sort(odds, Comparator.reverseOrder());
+        Arrays.sort(evens, Comparator.naturalOrder());
+
+        for (i = 0; i < nums.length / 2; i++) {
+            nums[2 * i] = evens[i];
+            nums[2 * i + 1] = odds[i];
+        }
+
+        if (nums.length % 2 == 1)
+            nums[2 * i] = evens[evens.length - 1];
+
+        return nums;
+    }
+}
---
+++ app/src/test/java/ir/home/tutorial/algorithm/leetcode/contest/weeklycontest279/p1/SolutionTest.java
@@ -0,0 +1,38 @@
+package ir.home.tutorial.algorithm.leetcode.contest.weeklycontest279.p1;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class SolutionTest {
+    private static Solution solution;
+
+    static Stream<Arguments> questionSampleArrays() {
+        return Stream.of(
+                arguments(new int[]{4, 1, 2, 3, 0}, new int[]{0, 3, 2, 1, 4}),
+                arguments(new int[]{4, 1, 2, 3}, new int[]{2, 3, 4, 1}),
+                arguments(new int[]{4, 1, 2}, new int[]{2, 1, 4}),
+                arguments(new int[]{2, 1}, new int[]{2, 1})
+        );
+    }
+
+
+    @BeforeAll
+    static void setUp() {
+        solution = new Solution();
+    }
+
+    @ParameterizedTest
+    @MethodSource("questionSampleArrays")
+    void testQuestionSampleArrays(final int[] nums, int[] expected) {
+        final var actual = solution.sortEvenOdd(nums);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
