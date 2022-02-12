71ae22e4b015bfaacb4c079b860cffe0c2bc68fe
myasinkaji <myasinkaji@gmail.com>
2022-02-12 14:28:35 +0330

    #problems 9. Palindrome Number MBR(Must be Refactored)

---
+++ app/src/test/java/ir/home/tutorial/algorithm/leetcode/questions/p9/SolutionTest.java
@@ -0,0 +1,37 @@
+package ir.home.tutorial.algorithm.leetcode.questions.p9;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class SolutionTest {
+    private static Solution solution;
+
+    @BeforeAll
+    static void setUp() {
+        solution = new Solution();
+    }
+
+    static Stream<Arguments> leetcodeSampleInput() {
+        return Stream.of(
+                arguments(121, true),
+                arguments(-121, false),
+                arguments(10, false),
+                arguments(1001001, true)
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("leetcodeSampleInput")
+    void testLeetcodeSampleInput(int num, boolean expected) {
+        final var actual = solution.isPalindrome(num);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
