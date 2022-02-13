2d98da69e4cdb77fbddef9c62e513b4cdabf4a54
myasinkaji <myasinkaji@gmail.com>
2022-02-13 06:35:13 +0330

    #weekly-contest-280 6004. Count Operations to Obtain Zero

--- app/src/main/java/ir/home/tutorial/algorithm/leetcode/contest/weekly/weeklycontest280/p1/Solution.java
+++ app/src/main/java/ir/home/tutorial/algorithm/leetcode/contest/weekly/weeklycontest280/p1/Solution.java
@@ -2,19 +2,16 @@ package ir.home.tutorial.algorithm.leetcode.contest.weekly.weeklycontest280.p1;
 
 class Solution {
     public int countOperations(int num1, int num2) {
-        if (num1 == 0 || num2 == 0)
-            return 0;
-        return count(num1, num2, 0);
-    }
-
-    private int count(int num1, int num2, int count) {
-        if (num1 == 0 || num2 == 0)
-            return count;
-
-        if (num1 >= num2)
-            num1 -= num2;
-        else num2 -= num1;
-
-        return count(num1, num2, ++count);
+        int count = 0;
+        while (num1 != 0 && num2 != 0) {
+            if (num1 >= num2) {
+                count += num1 / num2;
+                num1 -= num1 / num2 * num2;
+            } else {
+                count += num2 / num1;
+                num2 -= num2 / num1 * num1;
+            }
+        }
+        return count;
     }
 }
