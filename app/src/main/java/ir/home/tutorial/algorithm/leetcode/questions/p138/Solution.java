552ccac61d4b6680f0155b030d31c8857bfac219
Mohammad Yasin Sadeghi <myasinkaji@gmail.com>
2022-03-12 19:40:13 +0330

    #problems(March Challenge-Day12) 138. Copy List with Random Pointer MBR (also refactor correspond test case)

--- app/src/main/java/ir/home/tutorial/algorithm/leetcode/questions/p138/Solution.java
+++ app/src/main/java/ir/home/tutorial/algorithm/leetcode/questions/p138/Solution.java
@@ -1,27 +1,29 @@
 package ir.home.tutorial.algorithm.leetcode.questions.p138;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 class Solution {
     public Node copyRandomList(final Node head) {
         final var nodeToIndex = new HashMap<Node, Integer>();
-        final var indexToNode = new HashMap<Integer, Node>();
+        final var indexToNode = new ArrayList<Node>();
         var tail = head;
         final var copyHead = new Node(0);
         var copyTail = copyHead;
-        int count = 0;
+        int count;
         for (count = 0; tail != null; count++) {
             copyTail.next = new Node(tail.val);
             copyTail = copyTail.next;
-            indexToNode.put(count, copyTail);
+            indexToNode.add(copyTail);
             nodeToIndex.put(tail, count);
             tail = tail.next;
         }
 
         copyTail = copyHead.next;
         tail = head;
-        for (int i = 0; tail != null; i++) {
-            copyTail.random = indexToNode.get(nodeToIndex.get(tail.random));
+        while (tail != null) {
+            if (tail.random != null)
+                copyTail.random = indexToNode.get(nodeToIndex.get(tail.random));
             copyTail = copyTail.next;
             tail = tail.next;
         }
