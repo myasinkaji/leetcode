0936f3bedc3ffb327738554c664e75a4fa514239
Mohammad Yasin Sadeghi <myasinkaji@gmail.com>
2022-03-17 01:49:08 +0330

    Refactoring 946. Validate Stack Sequences MBR

--- app/src/main/java/ir/home/tutorial/algorithm/leetcode/questions/p946/Solution.java
+++ app/src/main/java/ir/home/tutorial/algorithm/leetcode/questions/p946/Solution.java
@@ -1,8 +1,10 @@
 package ir.home.tutorial.algorithm.leetcode.questions.p946;
 
 class Solution {
+    private static int[] stack = new int[10];
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        final var stack = new int[pushed.length];
+        if (stack.length < pushed.length)
+            stack = new int[pushed.length];
         int index = -1;
         for (int i = 0, popIndex = 0; i < pushed.length; i++) {
             if (pushed[i] == popped[popIndex]) {
