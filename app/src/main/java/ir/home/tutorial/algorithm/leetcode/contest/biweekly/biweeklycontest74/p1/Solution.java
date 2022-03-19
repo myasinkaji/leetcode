5e2ef6322b588d2ff2b90bf4defa0d02f9593e63
Mohammad Yasin Sadeghi <myasinkaji@gmail.com>
2022-03-19 21:01:34 +0330

    Refactoring 6020. Divide Array Into Equal Pairs MBR

--- app/src/main/java/ir/home/tutorial/algorithm/leetcode/contest/biweekly/biweeklycontest74/p1/Solution.java
+++ app/src/main/java/ir/home/tutorial/algorithm/leetcode/contest/biweekly/biweeklycontest74/p1/Solution.java
@@ -1,22 +1,18 @@
 package ir.home.tutorial.algorithm.leetcode.contest.biweekly.biweeklycontest74.p1;
 
-import java.util.HashSet;
-
 class Solution {
     public boolean divideArray(int[] nums) {
-        final var map = new HashSet<>();
+        final var map = new int[501];
         int remain = 0;
-        int counter = 0;
         for (int i : nums) {
-            if (map.contains(i)) {
-                map.remove(i);
+            if (map[i] == 1) {
+                map[i]= 0;
                 remain--;
-                counter++;
             } else {
-                map.add(i);
+                map[i]++;
                 remain++;
             }
         }
-        return remain == 0 && (counter == (nums.length / 2));
+        return remain == 0;
     }
 }
